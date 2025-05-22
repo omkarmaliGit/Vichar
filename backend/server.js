0112f74e1d823d48554f63217224f6859c796a80
omkarmaliGit <omkarmali51@gmail.com>
2025-05-22 12:40:49 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend origin
+    origin: "http://localhost:5173" || "https://vicharsocial.vercel.app", // frontend origin
     credentials: true, // if you're using cookies
   })
 );
