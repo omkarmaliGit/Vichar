ea6c3787c21446a8c6f362833f3980109689d68c
omkarmaliGit <omkarmali51@gmail.com>
2025-05-22 12:09:28 +0530

    vite

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-const backendUrl = import.meta.env.BACKEND_URL;
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const USER_API_END_POINT = `${backendUrl}/api/user`;
 export const VICHAR_API_END_POINT = `${backendUrl}/api/vichar`;
